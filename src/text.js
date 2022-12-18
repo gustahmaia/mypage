84591751de6e5ca9643f965ed95ee9fa3202ee3f
gusta <gusta@DESKTOP-18GC8UK.localdomain>
2022-12-18 00:56:09 -0300

    adding brazil and english language

--- src/text.js
+++ src/text.js
@@ -1,5 +1,5 @@
 const KEY_GUSTA_MYPAGE_LANG = "KEY_GUSTAMYPAGE_LANG";
-const KEY_GUSTA_MYPAGE_LANG_PT = "KEY_GUSTA_MYPAGE_LANG_PT";
+const KEY_GUSTA_MYPAGE_LANG_PT_BR = "KEY_GUSTA_MYPAGE_LANG_PT_BR";
 const KEY_GUSTA_MYPAGE_LANG_EN = "KEY_GUSTA_MYPAGE_LANG_EN";
 
 const texts = {
@@ -10,29 +10,44 @@ const texts = {
     },
     en: {
         title: `It's been 5 years since I ventured into the world of programming,
-        I hope I can help you with some of my projects!s`,
+        I hope I can help you with some of my projects!`,
         animWorking: "in development !!!"
     }
 };
 
-const init = function () {
+const clickLang = function () {
+    if (localStorage.getItem(KEY_GUSTA_MYPAGE_LANG) === KEY_GUSTA_MYPAGE_LANG_PT_BR) {
+        localStorage.removeItem(KEY_GUSTA_MYPAGE_LANG);
+        localStorage.setItem(KEY_GUSTA_MYPAGE_LANG, KEY_GUSTA_MYPAGE_LANG_EN);
+    } else {
+        localStorage.removeItem(KEY_GUSTA_MYPAGE_LANG);
+        localStorage.setItem(KEY_GUSTA_MYPAGE_LANG, KEY_GUSTA_MYPAGE_LANG_PT_BR);
+    }
+
+    initText();
+}
+
+const initText = function () {
 
     if (!localStorage.getItem(KEY_GUSTA_MYPAGE_LANG)) {
-        localStorage.setItem(KEY_GUSTA_MYPAGE_LANG, KEY_GUSTA_MYPAGE_LANG_PT);
+        localStorage.setItem(KEY_GUSTA_MYPAGE_LANG, KEY_GUSTA_MYPAGE_LANG_PT_BR);
     }
 
     langSelect = localStorage.getItem(KEY_GUSTA_MYPAGE_LANG);
 
     const spanTitle = document.getElementById("spanTitle");
     const spanAnimWorking = document.getElementById("spanAnimWorking");
+    const imgLang = document.getElementById("imgLang");
 
-    if (langSelect === KEY_GUSTA_MYPAGE_LANG_PT) {
+    if (langSelect === KEY_GUSTA_MYPAGE_LANG_PT_BR) {
         spanTitle.innerHTML = texts.pt.title;
         spanAnimWorking.innerHTML = texts.pt.animWorking;
+        imgLang.src = "/public/brasil.png";
     } else {
         spanTitle.innerHTML = texts.en.title;
         spanAnimWorking.innerHTML = texts.en.animWorking;
+        imgLang.src = "/public/eua.png";
     }
 }
 
-init();
+initText();
